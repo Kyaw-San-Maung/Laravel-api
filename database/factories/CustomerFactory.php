f92730c86bcc1b026d73c552b0191e1e4e9f1a58
Kyaw San Maung <kyawsanmaung262019@gmail.com>
2024-05-29 16:33:09 +0630

    feat: add fake data.

--- database/factories/CustomerFactory.php
+++ database/factories/CustomerFactory.php
@@ -16,8 +16,19 @@ class CustomerFactory extends Factory
      */
     public function definition(): array
     {
+        $type = $this->faker->randomElement(['I', 'B']); //Individual or Business
+        $name = $type == 'I' ?  $this->faker->name() : $this->faker->company();
+
+        $states = $this->faker->randomElement(['Yangon', 'Mandalay']);
+
         return [
-            //
+            'name' => $name,
+            'type' => $type,
+            'email' => $this->faker->unique()->safeEmail(),
+            'address' => $this->faker->streetAddress(),
+            'city' => $this->faker->city(),
+            'state' => $states,
+            'postal_code' => $this->faker->postcode()
         ];
     }
 }
